60eb405dd97f596811f5f77beb4a914c8375bc8b
Tanvir Ahmed <tanvira@inneed.cloud>
2022-08-18 01:25:18 +0600

    init

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You have successfully deployed your app.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://tanvirewu.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my profile
         </a>
       </header>
     </div>
